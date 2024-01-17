f11fc508dc73834274a6cea510fe1ab2104aabd9
shoeb7558 <shoeb9530@gmail.com>
2024-01-17 22:46:54 +0530

    first commit

---
+++ reduxDemo.jsx
@@ -0,0 +1,29 @@
+
+const redux = require('redux');
+
+const counterReducer = (state = {counter: 0}, action) =>{
+  if (action.type === 'increment'){
+   return {
+        counter: state.counter + 2
+    }
+}
+if (action.type === 'decrement'){
+    return {
+         counter: state.counter - 2
+     }
+ }
+}
+
+
+const store = redux.createStore(counterReducer);
+
+
+const counterSubscriber = () => {
+    const latestState = store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({type: 'increment'})
+store.dispatch({type: 'decrement'})
